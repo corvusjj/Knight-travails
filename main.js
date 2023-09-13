ffeb9fc3280a50894dc48b4bb5a71d7190d14201
John <mjohncorbet@gmail.com>
2023-09-13 23:01:54 +0800

    set travelKnight algorithm

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ class chessGraph {
     constructor() {
         this.visitedSquares = new Map();
         this.currentLevel = [];
+        this.found = false;
+        this.targetSquare = '';
     }
 
     //  vertex
@@ -9,31 +11,35 @@ class chessGraph {
         this.visitedSquares.set(s, []);
     }
 
-    //  connection between squares
+    //  connection between squares, first index will be the parent
     addEdge(s1, s2) {
         this.visitedSquares.get(s1).push(s2);
         this.visitedSquares.get(s2).push(s1);
     }
 
-    knightMoves(x,y, fn) {
+    knightMoves(x, y, fn) {
         //  top and right directions
-        fn(x-1, y+2);
-        fn(x+1, y+2);
-        fn(x+2, y+1);
-        fn(x+2, y-1);
+        fn(x-1, y+2, `${x}${y}`);
+        fn(x+1, y+2, `${x}${y}`);
+        fn(x+2, y+1, `${x}${y}`);
+        fn(x+2, y-1, `${x}${y}`);
     
         //  bottom and left directions
-        fn(x+1, y-2);
-        fn(x-1, y-2);
-        fn(x-2, y-1);
-        fn(x-2, y+1);
+        fn(x+1, y-2, `${x}${y}`);
+        fn(x-1, y-2, `${x}${y}`);
+        fn(x-2, y-1, `${x}${y}`);
+        fn(x-2, y+1, `${x}${y}`);
     }
 
-    discoverSquares = (x, y) => {
+    //  method for seeking the 8 squares the knight can move, adding those
+    //  squares the knight haven't visited yet.
+    discoverSquares = (x, y, parent) => {
         if (x < 0 || x > 7) return;
         if (y < 0 || y > 7) return;
         x = `${x}`;
         y = `${y}`;
+
+        if (this.targetSquare === x+y) return this.found = true;
 
         // prevent repeating squares to visitedSquares array.
         let alreadyVisited = false;
@@ -47,11 +53,37 @@ class chessGraph {
         }
     
         if (alreadyVisited === true) return;
+
         this.addSquare(x+y);
+        this.addEdge(x+y, parent);
+        this.currentLevel.push(x+y);
     }
 
     travelKnight(x,y) {
-        this.knightMoves(x,y, this.discoverSquares);
+        this.addSquare(`${x}${y}`);
+        this.knightMoves(x,y, this.discoverSquares);  //  1st level order knight moves
+        if (this.found === true) return console.log('found');
+
+        const nextLevelOrder = () => {
+            const nextSquares = [...this.currentLevel];
+            this.currentLevel.length = 0;
+
+            let i = 0;
+            while(i < nextSquares.length) {
+                if (this.found === true) return console.log('found');
+
+                const x = parseInt(nextSquares[i][0]);
+                const y = parseInt(nextSquares[i][1]);
+
+                console.log(x,y);
+                this.knightMoves(x,y, this.discoverSquares);
+                i++;
+            }
+
+            if (this.found === false) return nextLevelOrder();
+        }
+   
+        return nextLevelOrder();
     }
 
     printGraph() {
@@ -72,7 +104,6 @@ class chessGraph {
 
 const board = new chessGraph();
 
-board.travelKnight(0, 6);
+board.targetSquare = '77';
 board.travelKnight(3, 3);
 board.printGraph();
-
