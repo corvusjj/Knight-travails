659bec6ae47d17fe315848d3e9087d5501e92e74
John <mjohncorbet@gmail.com>
2023-09-13 21:02:49 +0800

    set graph for chessBoard

--- main.js
+++ main.js
@@ -1,41 +1,78 @@
-const visitedSquares = [];
-
-const knightMoves = (x,y, fn) => {
-    //  top and right directions
-    fn(x-1, y+2);
-    fn(x+1, y+2);
-    fn(x+2, y+1);
-    fn(x+2, y-1);
-
-    //  bottom and left directions
-    fn(x+1, y-2);
-    fn(x-1, y-2);
-    fn(x-2, y-1);
-    fn(x-2, y+1);
-}
-
-const discoverSquares = (x, y) => {
-    if (x < 0 || x > 7) return;
-    if (y < 0 || y > 7) return;
-    
-    // prevent repeating squares to visitedSquares array.
-    let alreadyVisited = false;
-
-    for (const square of visitedSquares) {
-        if (square.toString() === [x,y].toString()) {
-            alreadyVisited = true;
-            break;
-        }
+class chessGraph {
+    constructor() {
+        this.visitedSquares = new Map();
+        this.currentLevel = [];
     }
 
-    if (alreadyVisited === true) return;
-    visitedSquares.push([x,y]);
+    //  vertex
+    addSquare(s) {
+        this.visitedSquares.set(s, []);
+    }
+
+    //  connection between squares
+    addEdge(s1, s2) {
+        this.visitedSquares.get(s1).push(s2);
+        this.visitedSquares.get(s2).push(s1);
+    }
+
+    knightMoves(x,y, fn) {
+        //  top and right directions
+        fn(x-1, y+2);
+        fn(x+1, y+2);
+        fn(x+2, y+1);
+        fn(x+2, y-1);
+    
+        //  bottom and left directions
+        fn(x+1, y-2);
+        fn(x-1, y-2);
+        fn(x-2, y-1);
+        fn(x-2, y+1);
+    }
+
+    discoverSquares = (x, y) => {
+        if (x < 0 || x > 7) return;
+        if (y < 0 || y > 7) return;
+        x = `${x}`;
+        y = `${y}`;
+
+        // prevent repeating squares to visitedSquares array.
+        let alreadyVisited = false;
+        const getSquares = this.visitedSquares.keys();
+
+        for (const square of getSquares) {
+            if (square === x+y) {
+                alreadyVisited = true;
+                break;
+            }
+        }
+    
+        if (alreadyVisited === true) return;
+        this.addSquare(x+y);
+    }
+
+    travelKnight(x,y) {
+        this.knightMoves(x,y, this.discoverSquares);
+    }
+
+    printGraph() {
+        const getKeys = this.visitedSquares.keys();
+
+        for (const i of getKeys) {
+            const getValues = this.visitedSquares.get(i);
+            let conc = '';
+
+            getValues.forEach(v => {
+                conc += v + ' ';
+            });
+
+            console.log(i + '->' + conc);
+        }
+    }
 }
 
-const travelKnight = (x,y) => {
-    knightMoves(x,y, discoverSquares);
-}
+const board = new chessGraph();
 
-travelKnight(0, 6);
-travelKnight(3, 3);
-console.log(visitedSquares);
+board.travelKnight(0, 6);
+board.travelKnight(3, 3);
+board.printGraph();
+
